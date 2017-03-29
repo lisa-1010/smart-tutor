0f24c1a91cf512dabcec8a5df83853963f489077
Daniel <you@example.com>
2017-03-29 11:27:13 -0400

    Added placeholder for MCTS implementation and testing out git

---
+++ mcts.py
@@ -0,0 +1,21 @@
+#===============================================================================
+# DESCRIPTION:
+# An implementation of Monte Carlo Tree Search for POMDPs (TODO ref)
+# Takes in a simulator (model) which can generate samples, and maintains a policy.
+#===============================================================================
+
+#===============================================================================
+# CURRENT STATUS: Barely starting
+#===============================================================================
+
+import numpy as np
+import scipy as sp
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
